080dafc31ec3872f1f31a1c75a67088efb741c8d
ViliusBundulas <vilius.bundulas@gmail.com>
2021-06-15 10:59:50 +0300

    WIP: Added login view controller
    Removed swiftUI implementation

---
+++ GitAcademy/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  GitAcademy
+//
+//  Created by Vilius Bundulas on 2021-06-15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
